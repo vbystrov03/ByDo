2c354bb0f5ee123f7cbf89acd9938bdece14c309
Владимир Быстров <vladimir.bystrov@galaktika.ru>
2025-04-07 09:34:22 +0300

    Update Files

---
+++ API-Gateway-Service/Gateway.py
@@ -0,0 +1,9 @@
+import fastapi as fa
+
+services = {
+    "Authentification": "http://localhost:10000",
+    "Task": "http://localhost:10001", 
+    "Notification": "http://localhost:10002"
+    "Analytics": "http://localhost:10003"
+    "Profils": "http://localhost:10004"
+}
